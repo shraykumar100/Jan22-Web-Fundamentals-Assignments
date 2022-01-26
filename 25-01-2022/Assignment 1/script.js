e8c2c81e6c289aa29349b4a5a76cf3d1bcac1a47
Shray <shraykumar100@gmail.com>
2022-01-26 14:15:22 +0530

    25 jan assignment

--- 25-01-2022/Assignment 1/script.js
+++ 25-01-2022/Assignment 1/script.js
@@ -1,6 +1,8 @@
 var num = 50;
-if (num >= 10) {
-    console.log("The number is greater than or equal to 10");
+if (num > 10) {
+    console.log("The number is greater than 10");
+} else if (num === 10) {
+    console.log("The number is equal to 10");
 } else {
-    console.log("The number is not greater than or equal to 10");
+    console.log("num is niether greater than nor equal to 10");
 }
