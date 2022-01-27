ea0df2029e11588b0e64d3959e1609bdcd817501
Shray <shraykumar100@gmail.com>
2022-01-27 19:33:57 +0530

    26 jan assignment

---
+++ 26-01-2022/Assignment 1/script.js
@@ -0,0 +1,17 @@
+var num1 = prompt("Enter first number:");
+var num2 = prompt("Enter second number:");
+var num3 = prompt("Enter third number:");
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+num3 = parseInt(num3);
+var min;
+if (num1 < num2) {
+    min = num1;
+} else {
+    min = num2;
+}
+if (min > num3) {
+    min = num3;
+}
+alert(min);
+
---
+++ 26-01-2022/Assignment 2/script.js
@@ -0,0 +1,6 @@
+var char = prompt("Enter any alphabet:");
+if (char == "a" || char == "A" || char == "e" || char == "E" || char == "i" || char == "I" || char == "o" || char == "O" || char == "u" || char == "U") {
+    alert("Entered alphabet is a vowel!");
+} else {
+    alert("Entered alphabet is a consonent!");
+}
