6a2769af356916ef1cfa4fb1ca20d2f407741a03
Shray <shraykumar100@gmail.com>
2022-01-28 18:33:19 +0530

    27 jan assignment

---
+++ 27-01-2022/script.js
@@ -0,0 +1,15 @@
+let arr = [];
+let max = 0;
+let inputNum = Number(prompt("Enter how many numbers you want to give me."));
+for (let i = 0; i < inputNum; i++) {
+    var numbers = Number(prompt("Enter a number:"));
+    arr.push(numbers);
+}
+console.log(`Your new array is [${arr}]`);
+
+for (let j = 0; j < arr.length; j++) {
+    if (arr[j] > max) {
+        max = arr[j];
+    }
+}
+console.log(`The largest number in array is ${max}`);
