90e4bd2d658774fd1a511321e310dc3c23ce1b69
shraykumar <shraykumar100@gmail.com>
2022-03-04 00:57:02 +0530

    minor changes

--- March/Week-1/03-03-2022/index.js
+++ March/Week-1/03-03-2022/index.js
@@ -1,8 +1,14 @@
-var a = parseInt(prompt("Enter value of a :"));
-var b = parseInt(prompt("Enter value of b :"));
+var a = prompt("Enter value of a :");
+var b = prompt("Enter value of b :");
 
-a = a - b;
-b = a + b;
-a = -(a) + b;
-var answer = document.getElementById("ans");
-answer.innerText = (`Answer : The swaped value of a is ${a} and b is ${b}.`);
+if (isNaN(a) || isNaN(b)) {
+    alert("Please enter valid numbers!!!")
+} else {
+    a = parseInt(a);
+    b = parseInt(b);
+    a = a - b;
+    b = a + b;
+    a = -(a) + b;
+    var answer = document.getElementById("ans");
+    answer.innerText = (`Answer : The swaped value of a is ${a} and b is ${b}.`);
+}
